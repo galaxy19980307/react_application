f08f8f27449eeb706a3a95ad02ebd151069c4b5b
galaxy19980307 <ukrysenko03071998@gmail.com>
2023-02-14 15:30:11 +0300

    Запостила свое фото

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -6,6 +6,7 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div className={s.avatarDescription}>
+                <img src={"https://sun9-33.userapi.com/impg/b_R4ZHwyMIYCcFw8iMH6v15PaoLpAOMtgGgDvQ/_wJK3hLQ1ls.jpg?size=1620x2160&quality=96&sign=612e5ac298f9b8e8471d2b9cad98486c&type=album"}/>
                 <img src={props.profile.photos.large}/>
             </div>
             <div className={s.fullName}>
